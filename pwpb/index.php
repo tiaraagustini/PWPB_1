bd7126598ded0815d47f6fae754cffc29629f99f
tiaraagustini <53927140+tiaraagustini@users.noreply.github.com>
2019-08-13 10:49:01 +0700

    menambahkan tugas pwpb

---
+++ pwpb/index.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Tugas pwpb</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/pricing/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <style>
+      .pengguna{
+        width: 150px;
+        height: 150px;
+        border-radius: 10px;
+      }
+      .card{
+        border-radius: 10px;
+      }
+        
+      
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="pricing.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">Tentang Kami</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="tugaspertama.php">TUGAS SATU</a>
+    <a class="p-2 text-dark" href="#">TUGAS DUA</a>
+    <a class="p-2 text-dark" href="#">TUGAS TIGA</a>
+    <a class="p-2 text-dark" href="#"> TUGAS EMPAT</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="index.php">Beranda</a>
+</div>
+
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <img class="foto" src="img/foto.jpg">
+  <h1 class="display-4">HELLO!</h1>
+  <p class="lead">Selamat datang dihalaman kami :)</p>
+</div>
+
+
+
+  </footer>
+</div>
+</body>
+</html>
